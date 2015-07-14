d939e40826efce76469949aafef01aeed1aedc25
Mihai Banulescu <mihai.banulescu@gmail.com>
2015-07-14 12:11:29 -0700

    small fixes

--- app/controllers/comp_tiers_controller.rb
+++ app/controllers/comp_tiers_controller.rb
@@ -1,28 +1,22 @@
 class CompTiersController < ApplicationController
   before_action :set_comp_tier, only: [:show, :edit, :update, :destroy]
 
-  # GET /comp_tiers
-  # GET /comp_tiers.json
   def index
     @comp_tiers = CompTier.all
+    @tiers = []
+    (1..6).each { |n| @tiers.push CompTier.tier(n)}
   end
 
-  # GET /comp_tiers/1
-  # GET /comp_tiers/1.json
   def show
   end
 
-  # GET /comp_tiers/new
   def new
     @comp_tier = CompTier.new
   end
 
-  # GET /comp_tiers/1/edit
   def edit
   end
 
-  # POST /comp_tiers
-  # POST /comp_tiers.json
   def create
     @comp_tier = CompTier.new(comp_tier_params)
 
@@ -37,8 +31,6 @@ class CompTiersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comp_tiers/1
-  # PATCH/PUT /comp_tiers/1.json
   def update
     respond_to do |format|
       if @comp_tier.update(comp_tier_params)
@@ -51,8 +43,6 @@ class CompTiersController < ApplicationController
     end
   end
 
-  # DELETE /comp_tiers/1
-  # DELETE /comp_tiers/1.json
   def destroy
     @comp_tier.destroy
     respond_to do |format|
@@ -62,12 +52,10 @@ class CompTiersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_comp_tier
       @comp_tier = CompTier.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comp_tier_params
       params.require(:comp_tier).permit(:name, :monthly_draw, :fixed_draw)
     end
--- app/models/comp_tier.rb
+++ app/models/comp_tier.rb
@@ -1,6 +1,11 @@
 class CompTier < ActiveRecord::Base
+  has_many :badge_sets
   
   def name_and_monthly_draw
     "#{name} - $#{monthly_draw}"
+  end
+  
+  def self.tier(n)
+    CompTier.all.select{ |t| t.name.starts_with?(n.to_s) }
   end
 end
