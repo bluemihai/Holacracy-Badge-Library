3f5b2f39873c83ace11f10b3537993bcddc357c6
Mihai Banulescu <mihai.banulescu@gmail.com>
2015-09-10 06:55:17 -0700

    show all badges initially

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         autoWidth: false,
         ordering: false,
         searching: false,
-        pageLength: 10,
+        pageLength: -1,
         lengthMenu: [ [10, 15, 20, 25, -1], [10, 15, 20, 25, "All"] ],
         language: { lengthMenu: "Display _MENU_ Badges" }
     });
