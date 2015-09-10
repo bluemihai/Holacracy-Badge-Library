52c69d314b5b109165f53810f4a95ac2fe34e9cc
Mihai Banulescu <mihai.banulescu@gmail.com>
2015-09-10 06:24:45 -0700

    fix %hri and add nominator to new validation page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? || Rails.env.test?
   provider :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret, {access_type: 'online', approval_prompt: ''}
 end
